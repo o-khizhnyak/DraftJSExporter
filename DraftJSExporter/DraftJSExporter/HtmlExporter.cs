1d408f6e765e7f2be1085d72748958ab400918bd
o.khizhnyak <oxanakhizhnyak@gmail.com>
2019-11-18 09:47:40 +0300

    wip separate html export

--- DraftJSExporter/DraftJSExporter/HtmlExporter.cs
+++ DraftJSExporter/DraftJSExporter/HtmlExporter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text;
 
 namespace DraftJSExporter
@@ -24,8 +25,9 @@ namespace DraftJSExporter
             bool parentIsInline, bool parentIsLastChild)
         {
             string tagName;
+            IReadOnlyDictionary<string, string> attributes = null;
             var selfClosing = false;
-            
+
             switch (node.Type)
             {
                 case TreeNodeType.Block:
@@ -38,15 +40,39 @@ namespace DraftJSExporter
                     var element = _config.EntityDecorators[node.Name](node.Data); 
                     tagName = element.Type;
                     selfClosing = element.SelfClosing;
+                    attributes = element.Attributes;
                     break;
                 default:
                     tagName = "";
                     break;
             }
             
-            TagBuilder.AddOpeningTag(sb, tagName, node.Data, level, node.IsInline, addTab, selfClosing);
+            TagBuilder.AddOpeningTag(sb, tagName, attributes, level, node.IsInline, addTab, selfClosing);
             
-            
+            if (selfClosing)
+            {
+                TagBuilder.CloseTag(sb, parentIsLastChild);
+            }
+            else
+            {
+                if (node.Text != null)
+                {
+                    TagBuilder.AddText(sb, node.Text, node.IsInline, level);         
+                }
+                
+                var inline = false;
+                var childrenCount = node.Children.Count;
+                for (var i = 0; i < childrenCount; i++)
+                {
+                    var child = node.Children[i];
+                    RenderElement(sb, child, node.Name == null && node.Text == null ? level : level + 1, 
+                        !(inline && child.IsInline) && !(child.Name == null && !child.IsInline) && !node.IsInline, 
+                        i == childrenCount - 1, node.IsInline, lastChild);
+                    inline = child.IsInline;
+                }
+
+                TagBuilder.AddClosingTag(sb, tagName, level, node.IsInline, lastChild, parentIsInline);
+            }
         }
     }
 }
